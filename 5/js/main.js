37aa914f16c1be7fd716b7473cd3a791be8100d7
Oleg <olegpodkolzin@icloud.com>
2021-09-18 03:18:45 +0300

    lab 5

--- 5/js/main.js
+++ 5/js/main.js
@@ -1,109 +1,189 @@
 function Manager(){
-    this.lang = 'eng';
-    this.week = 5;
+    this.lang = null;
+    this.week = null;
+    this.count = null;
     this.actions = [];
+
+    this.text = '';
+    this.day = -1;
+
+
+    this.DOM = {
+        lang: document.querySelector('select[name="lang"]'),
+        week: document.querySelector('select[name="week"]'),
+        count: document.querySelector('select[name="count"]'),
+        form: document.querySelector('form'),
+        text: document.querySelector('input[name="text"]'),
+        reset: document.querySelector('.setting__reset'),
+        daySelector: document.querySelector('select[name="day"]'),
+        list: document.querySelector('.list'),
+        submit: document.querySelector('.settings-submit'),
+        reset: document.querySelector('.setting-reset')
+    };
 
     if(!!localStorage.getItem('data')){
         let data = JSON.parse(localStorage.getItem('data'));
 
+        console.log(data);
+
         this.lang = data.lang;
         this.week = data.week;
+        this.count = data.count;
         this.actions = data.actions;
 
-        if(!this.actions.length){
-            this.disabledSettings(false);
-        }else{
-            this.disabledSettings(true);
-        }
-        
+        this.DOM.lang.value = this.lang;
+        this.DOM.week.value = this.week;
+        this.DOM.count.value = this.count;
     }
 
-    document.querySelector('form').addEventListener('submit', (event) => {
-        event.preventDefault();
+    const _updateValue = (forceUpdate) => {
+        let prev = {
+            lang: this.lang,
+            week: this.week,
+            count: this.count
+        };
 
+        this.lang = this.DOM.lang.value;
+        this.week = Math.abs(parseInt(this.DOM.week.value));
+        this.count = parseInt(this.DOM.count.value);
 
-        this.disabledSettings(true);
+        this.text = this.DOM.text.value;
+        this.day = parseInt(this.DOM.daySelector.value);
 
-        this.addAction(document.querySelector('textarea').value);
-        document.querySelector('textarea').value = '';
-        this.render();
-        this.save();
-    });
+        if(this.lang != prev.lang || this.week != prev.week || this.count != prev.count || forceUpdate){
+            this.actions = new Array(this.week).fill(0);
+            this.actions.forEach((item, index) => {
+                this.actions[index] = new Array();
+            });
+        }
 
-    document.querySelector('select[name="week"]').addEventListener('change', (event) => {
-        this.week = event.target.value;
-        this.render();
-        this.save();
-    });
+        _stateSettings(!!this.actions.reduce((prev, cur) => [...prev, ...cur]).length);
+        _stateSubmit(!(!!this.text && !(this.actions[this.day - 1].length == this.count)));
+        _render(this.week != prev.week);
 
-    document.querySelector('select[name="lang"]').addEventListener('change', (event) => {
-        this.lang = event.target.value;
-        this.render();
-        this.save();
-    });
+        _save();
+    };
 
-    document.querySelector('textarea').addEventListener('input', (event) => {
-        document.querySelector('input[type="submit"]').disabled = event.target.value == '' || this.actions.length == this.week;
-    });
+    const _reset = () => {
+        document.querySelector('form').reset();
+        this.actions = [];
+        
+        _stateSettings(false);
+        _stateSubmit(false);
 
-    document.querySelector('button').addEventListener('click', (event) => {
-        this.reset();
-        this.render();
-        this.save();
-    });
-}
+        _updateValue(true);
+        _render(true);
+    };
 
-Manager.prototype.disabledSettings = function(state){
-    document.querySelector('select[name="week"]').disabled = state;
-    document.querySelector('select[name="lang"]').disabled = state;
-    document.querySelector('input[type="submit"]').disabled = state;
-}
+    const _add = (text, day) => {
+        if(text == '') return;
 
-Manager.prototype.reset = function(){
-    this.lang = 'eng';
-    this.week = 5;
-    this.actions = [];
+        _stateSettings(true);
 
-    this.disabledSettings(false);
-};
+        day -= 1;
+    
+        if(day < 0 || day >= this.week) return;
+        if(this.actions[day].length == this.count) return;
 
-Manager.prototype.save = function(){
-    localStorage.setItem('data', JSON.stringify(this));
-};
+        this.actions[day].push({name: text});
 
-Manager.prototype.addAction = function(text){
-    if(this.actions.length < this.week){
-        this.actions.push({
-            action: text
+        _updateValue();
+    }
+
+    const _stateSettings = (state) => {
+        if(state){
+            document.querySelector('.setting-params__settings').classList.add('setting-params__settings--off')
+        }else{
+            document.querySelector('.setting-params__settings').classList.remove('setting-params__settings--off')
+        }
+
+        this.DOM.week.disabled = state;
+        this.DOM.lang.disabled = state;
+        this.DOM.count.disabled = state;
+    }
+
+    const _stateSubmit = (state) => {
+        this.DOM.submit.disabled = state;
+    }
+
+    const _render = (weekChanged) => {
+
+        if(weekChanged){
+            let prevDay = this.DOM.daySelector.value;
+            this.DOM.daySelector.innerHTML = new Array(this.week).fill(0).map((item, index) => `<option value="${index + 1}">${index + 1}</option>`);
+            this.DOM.daySelector.value = prevDay;
+
+            this.DOM.list.innerHTML = '';
+        }
+
+        
+        new Array(this.actions.length).fill(0).forEach((_, dayIndex) => {
+            
+            if(!!!document.querySelector(`[data-day="${dayIndex}"]`)){
+                let dayNode = document.createElement('div');
+
+                dayNode.className ='list-day';
+                dayNode.dataset.day = dayIndex;
+
+                dayNode.innerHTML = `
+                    <h1 class="list-day__name">День ${dayIndex + 1}</h1>
+                    <div class="list-day__content">
+
+                    </div>
+                `;
+
+                this.DOM.list.append(dayNode);
+            }
+
+            this.actions[dayIndex].forEach((action, actionIndex) => {
+                if(!!!document.querySelector(`[data-day="${dayIndex}"] [data-action="${actionIndex}"]`)){
+                    let actionNode = document.createElement('div');
+
+                    actionNode.className ='list-item';
+                    actionNode.dataset.action = actionIndex;
+
+                    actionNode.innerHTML = `
+                        <div class="list-item__id">День ${dayIndex + 1} Задание ${actionIndex + 1}</div>
+                        <div class="list-item__name">${action.name}</div>
+                    `;
+
+                    document.querySelector(`[data-day="${dayIndex}"] > .list-day__content`).append(actionNode);
+                }
+            });
         });
     }
-};
 
-Manager.prototype.getActions = function(){
-    let items = [...this.actions];
+    const _save = () => {
+        localStorage.setItem('data', JSON.stringify({
+            ...this,
+            DOM: null
+        }));
+    };
 
-    for(let i = items.length; i < this.week; i++){
-        items.push({action: ''});
-    }
+    this.DOM.form.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-    return items;
-};
+        _add(this.DOM.text.value, parseInt(this.DOM.daySelector.value));
+        _render();
+    });
 
-Manager.prototype.render = function(){
-    document.querySelector('select[name="lang"]').value = this.lang;
-    document.querySelector('select[name="week"]').value = this.week;
+    this.DOM.week.addEventListener('change', () => _updateValue());
+    this.DOM.count.addEventListener('change', () => _updateValue());
+    this.DOM.lang.addEventListener('change', () => _updateValue());
+    this.DOM.daySelector.addEventListener('change', () => _updateValue());
+    this.DOM.text.addEventListener('input', () => _updateValue());
 
-    document.querySelector('.list').innerHTML = this.getActions().map((item, index) => {
-        return `
-            <div class="list-item">
-                <div class="list-item__day">${index + 1}</div>
-                <div class="list-item__action">${item.action == '' ? 'Свободный день' : item.action}</div>
-            </div>
-        `;
-    }).join('');
-};
+    this.DOM.reset.addEventListener('click', (event) => {
+        _reset();
+    });
+
+
+
+    _updateValue();
+    _render(true);
+}
 
 window.onload = () => {
     var man = new Manager();
-    man.render();
+    // man.render();
 };
